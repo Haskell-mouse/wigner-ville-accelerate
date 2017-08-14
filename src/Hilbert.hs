2a066251b17e0de2958b1e112bf85d61aca8be9b
Haskell-mouse <lazybonesxp@gmail.com>
2017-08-14 22:36:48 +0300

    Added new versions of accelerate-fft and others. Improved perfomance of hilbert transfrormation.

--- src/Hilbert.hs
+++ src/Hilbert.hs
@@ -5,34 +5,45 @@ import Data.Array.Accelerate.Array.Sugar as S
 import qualified Data.Array.Accelerate.Math.FFT as AMF
 import qualified Data.Array.Accelerate.Data.Complex as ADC
 
-hylbert :: A.Array A.DIM1 Double -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
-hylbert arr = 
-  let sh = shape arr
-      leng = S.size sh
-      hVect = h leng
-  in  inverseFFT (loadArrayFFt arr sh) sh hVect
+ezero :: A.Exp Double
+ezero = A.constant 0
+
+eone :: A.Exp Double
+eone = A.constant 1 
+
+etwo :: A.Exp Double 
+etwo = A.constant 2
+
+hilbert :: A.Acc (A.Array A.DIM1 Double) -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
+hilbert arr = 
+  let leng = A.length arr 
+      hVect = h (A.unit leng)
+  in  inverseFFT (loadArrayFFt arr) hVect
 
 -- | Scalar myltiplies our vector with h vector and make inverse FFT 
 
-inverseFFT :: A.Acc (A.Array A.DIM1 (ADC.Complex Double)) -> A.DIM1 -> A.Acc (A.Array A.DIM1 Double) -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
-inverseFFT arr dim1 h = AMF.fft1D' AMF.Inverse dim1 $ A.zipWith (A.*) arr (makeComplex h)
+inverseFFT :: A.Acc (A.Array A.DIM1 (ADC.Complex Double)) -> A.Acc (A.Array A.DIM1 Double) -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
+inverseFFT arr h = AMF.fft1D' AMF.Inverse (A.Z A.:. 0) $ A.zipWith (A.*) arr (makeComplex h)
 
 -- | Load vector to GPU, make it complex and apply FFT
 
-loadArrayFFt :: A.Array A.DIM1 Double -> DIM1 -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
-loadArrayFFt arr dim1 = AMF.fft1D' AMF.Forward dim1 $ makeComplex $ A.use arr
+applyFFt :: A.Acc (A.Array A.DIM1 Double) -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
+applyFFt arr = AMF.fft1D' AMF.Forward (A.Z A.:. 0) $ makeComplex arr
 
 -- | Form Vector that will be scalar multiplied with our spectre vector.
 
-h :: Int -> A.Acc (A.Array A.DIM1 Double)
-h size | (even size) && (size > 0) = A.generate (A.index1 (A.constant size)) (\ix -> let Z :. x = A.unlift ix in defEven x)
+h :: A.Acc (A.Scalar Int) -> A.Acc (A.Array A.DIM1 Double)
+h s = A.generate (A.index1 size) (\ix -> let Z :. x = A.unlift ix in def (A.fromIntegral x))
   where 
-  	defEven x = A.caseof x [(\y ->(y A.== (A.constant 0)) A.|| (y A.== (A.constant (size `div` 2))), A.constant 1),
-  	  (\y -> (x A.<= (A.constant (size `div` 2))), A.constant 2)] (A.constant 0)
-h size | otherwise                 = A.generate (A.index1 (A.constant size)) (\ix -> let Z :. x = A.unlift ix in defOdd x)
-  where
-    defOdd x = A.caseof x [((\y -> (y A.== (A.constant 0))), A.constant 1),
-      (\y -> (y A.>= (A.constant ((size+1) `div` 2))), A.constant 0)] (A.constant 2)
+    size = A.the s 
+    dsize = A.fromIntegral size
+    def x = A.ifThenElse (A.even size) (defEven x) (defOdd x)
+    defEven x = 
+      A.caseof x [
+      (\y ->(y A.== ezero) A.|| (y A.== (dsize/etwo)), eone),
+  	  (\y -> (x A.<= (dsize/etwo)), etwo)] ezero
+    defOdd x = A.caseof x [((\y -> (y A.== ezero)), eone),
+      (\y -> (y A.>= ((dsize A.+ eone)/etwo)), eone)] etwo
 
 -- | Make our array complex with zero imaginary part 
 
--- src/Wigner.hs
+++ src/Wigner.hs
@@ -18,7 +18,7 @@ import qualified Data.Array.Accelerate.IO as AI
 import qualified Data.Vector as V 
 
 
-wignerVille :: A.Array A.DIM1 (ADC.Complex Double) -> V.Vector (A.Array A.DIM1 Double)
+wignerVille :: A.Acc (A.Array A.DIM1 (ADC.Complex Double)) -> V.Vector (A.Array A.DIM1 Double)
 wignerVille arr = 
   let vectors = createRVectors arr
       dim1 = (A.Z A.:. (S.size $ shape arr))
@@ -60,49 +60,6 @@ createRVector arr t inds tau =
   in if (t <= leng `mround` 2) 
      then A.scatter arrInds (A.fill (A.constant sh) 0.0) v3
      else A.scatter arrInds (A.fill (A.constant sh) 0.0) v3r
-{-
-sh :: A.Array A.DIM1 (ADC.Complex Double) -> A.DIM1
-sh arr = shape arr
-
-leng :: A.DIM1 -> Int
-leng sh = S.size sh
-
-shT :: VS.Vector Int -> A.DIM1
-shT tau = (A.Z A.:. VS.length tau)
-
-arrInds :: VS.Vector Int -> ALP.Acc (A.Array A.DIM1 Int)
-arrInds inds = 
-  let shInds = shT inds
-  in A.use $ AI.fromVectors shInds inds
-
-accTau :: VS.Vector Int -> A.Acc (A.Array A.DIM1 Int)
-accTau tau = 
-  let shTau = shT tau 
-  in A.use $ AI.fromVectors shTau tau
-
-expT :: A.Exp Int
-expT = A.constant 5
-
-accI1 :: A.Acc (A.Array A.DIM1 Int) -> A.Acc (A.Array A.DIM1 Int)
-accI1 accTau = A.map ( + expT) accTau
-
-accI2 :: A.Acc (A.Array A.DIM1 Int) -> A.Acc (A.Array A.DIM1 Int)
-accI2 accTau =  A.map ((-) expT) accTau
-
-conjAccArr :: A.Acc (A.Array A.DIM1 (ADC.Complex Double)) -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
-conjAccArr accArr = A.map ADC.conjugate accArr
-
-v :: A.Acc (A.Array A.DIM1 Int) -> A.DIM1 -> A.Acc (A.Array A.DIM1 (ADC.Complex Double)) -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
-v accI1 sh accArr = A.scatter accI1 (A.fill (A.constant sh) 0.0) accArr
-
-v3 :: A.Acc (A.Array A.DIM1 (ADC.Complex Double)) -> A.Acc (A.Array A.DIM1 (ADC.Complex Double)) -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
-v3 v1 v2 = A.zipWith (*) v1 v2 -}
-{-
-v4 :: A.Acc (A.Array A.DIM1 Int) -> A.Acc (A.Array A.DIM1 Int) -> A.Acc (A.Array A.DIM1 Int)
-v4 tau arr = 
-  let zeros = A.fill (A.shape arr) 0
-  in
-  A.permute (+) zeros (\ix -> if (ix <= A.shape tau) then A.index1 (tau A.! ix) else ix) arr -}
 
 
 createParamsStruct :: A.Array A.DIM1 (ADC.Complex Double) -> V.Vector (Int, VS.Vector Int, VS.Vector Int) -- V.Vector (ALN.Acc (A.Array A.DIM1 (ADC.Complex Double)))
@@ -114,50 +71,12 @@ createParamsStruct arr =
       indicess = V.force $ V.map (indices leng) taus
   in V.zipWith3 (,,) ts indicess taus
 
-createRVectors :: A.Array A.DIM1 (ADC.Complex Double) -> V.Vector (ALN.Acc (A.Array A.DIM1 (ADC.Complex Double)))
+createRVectors :: A.Array A.DIM1 (ADC.Complex Double) -> V.Vector (A.Acc (A.Array A.DIM1 (ADC.Complex Double)))
 createRVectors arr =V.force $ V.map ( uncurry3 (createRVector arr)) $ createParamsStruct arr
-
---fftMap :: V.Vector (A.Acc (A.Array A.DIM1 (ADC.Complex Double))) -> V.Vector (A.Acc (A.Array A.DIM1 (ADC.Complex Double)))
--- fftMap 
 
 uncurry3 :: (a -> b -> c -> d) -> ((a, b, c) -> d)
 uncurry3 f ~(a,b,c) = f a b c 
-{-
-ind :: Int -> VS.Vector Int ->  VS.Vector Int
-ind leng tau = VS.map (\x -> ((x + leng) `mod` (leng)) + 1) tau
 
-createRVector :: VS.Vector Double -> Int -> R.Array R.D R.DIM2 Double
-createRVector xs t = let leng = VS.length xs
-                   in R.fromFunction (R.Z R.:. leng R.:. leng) (\(R.Z R.:. x R.:. t) -> create x t leng)
-                    where
-                    	create x t lengt = createElements (x+1) t lengt xs
-
-createGPU :: R.Array R.D R.DIM2 Double -> A.Acc (A.Array A.DIM0 Double) -> A.Acc (A.Array A.DIM2 (ADC.Complex Double))
-createGPU rArr zero = let pp :: (R.Array AI.A R.DIM2 Double)
-                          (Just pp) =  (AI.computeAccP rArr)
-                      in  A.map (\x -> ADC.mkPolar x (A.the zero)) $ A.use $ AI.fromRepa pp
-
---createFFT :: A.Acc (A.Array A.DIM1 (ADC.Complex Double)) -> A.Acc (A.Array A.DIM Int) -> A.Acc (A.Array A.DIM1 (ADC.Complex Double))
---createFFT arr leng = let dim1 = (A.Z A.:. leng A.:. leng)
---                     in AMF.fft1D' AMF.Forward dim1 array 
-
-createElements :: Int -> Int -> Int -> VS.Vector Double -> Double
-createElements x t leng dat = let !taum = taumax t leng
-                                  !tu = tau taum
-                                  !indexes = ind leng tu
-                                  !half = mround leng 2
-                              in case (VS.elemIndex x indexes) of Nothing -> if ((leng - half) >= t)&&(t >(half))&&((x==half+1))
-                              	                                             then middleElem half dat t
-                              	                                             else 0.0 
-                                                                  (Just n) -> createElement (tu VS.! n) dat t
-
-middleElem :: Int -> VS.Vector Double -> Int -> Double
-middleElem half xs t = (xs VS.! (t + half - 1))*(xs VS.! (t - half -1))
-
-
-createElement :: Int -> VS.Vector Double -> Int -> Double
-createElement tau xs time = (xs VS.! (time + tau))*(xs VS.! (time - tau))
--}
 mround :: Int -> Int -> Int 
 mround a !b = let half = b `div` 2
              in if (a `mod` b) >= half
